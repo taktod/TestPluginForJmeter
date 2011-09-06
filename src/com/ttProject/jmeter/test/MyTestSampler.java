732a55df14ff486afd8fd9f60ce96978b4d3895e
Toda Takahiko <poepoemix@hotmail.com>
2011-09-06 22:02:12 +0900

    初回コミット

---
+++ src/com/ttProject/jmeter/test/MyTestSampler.java
@@ -0,0 +1,16 @@
+package com.ttProject.jmeter.test;
+
+import org.apache.jmeter.samplers.AbstractSampler;
+import org.apache.jmeter.samplers.Entry;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.testbeans.TestBean;
+
+public class MyTestSampler extends AbstractSampler implements TestBean {
+
+	@Override
+	public SampleResult sample(Entry paramEntry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
