75f223f9793d1c7f48209d06658940b4f2720e61
Toda Takahiko <poepoemix@hotmail.com>
2011-09-06 22:29:37 +0900

    プログラムにSampleResultの動作を追加。計測に必要な最低限のデータが追加されたとおもいます。

--- src/com/ttProject/jmeter/test/MyTestSampler.java
+++ src/com/ttProject/jmeter/test/MyTestSampler.java
@@ -5,12 +5,39 @@ import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testbeans.TestBean;
 
+/**
+ * テストでつくっているサンプラー
+ * @author taktod
+ */
 public class MyTestSampler extends AbstractSampler implements TestBean {
+	/** シリアルバージョンIDを追加 */
+	private static final long serialVersionUID = -1L;
 
+	/**
+	 * サンプリング動作
+	 */
 	@Override
 	public SampleResult sample(Entry paramEntry) {
-		// TODO Auto-generated method stub
-		return null;
+		SampleResult result = new SampleResult();
+		// 動作結果のところに追加する動作タイトル
+		result.setSampleLabel("TestResult");
+		// サンプリングスタート
+		result.sampleStart();
+		try {
+			// 時間のかかる本処理
+			Thread.sleep(500L);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		// サンプル終了
+		result.sampleEnd();
+		// 応答内容
+		result.setResponseData("Sample is ended with success...".getBytes());
+		// 応答データを文字列にしておく。
+		result.setDataType(SampleResult.TEXT);
+		// 成功失敗の印(成功にしておく)
+		result.setSuccessful(true);
+		return result;
 	}
-
 }
